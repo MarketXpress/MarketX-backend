bff2c09beeec5ca77584e8acd6b19721fd37e1db
emmanuel-adah <edogbanya02@gmail.com>
2026-07-23 19:32:41 +0100

    Integrate TypeOrmModule with Review entity into ReviewModule

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewsController } from './review.controller';
 import { ReviewsService } from './review.service';
+import { Review } from './entities/review.entity';
 
 @Module({
   controllers: [ReviewsController],
+  imports: [TypeOrmModule.forFeature([Review])],
   providers: [ReviewsService],
 })
 export class ReviewModule {}
