490f0e73b88f135ae9e41740efc773144be3cba5
Qoder-Undefined <kaytech37@gmail.com>
2026-08-22 11:39:52 +0100

    product-cache interceptor

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { createCache } from 'cache-manager';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
@@ -8,6 +9,7 @@ import { PricingService } from './services/pricing.service';
 import { ProductImagesController } from './product-images.controller';
 import { ProductPriceEntity } from './entities/product-price.entity';
 import { Product } from '../entities/product.entity';
+import { ProductCacheInterceptor, PRODUCT_CACHE } from './product-cache.interceptor';
 
 @Module({
   imports: [
@@ -18,7 +20,15 @@ import { Product } from '../entities/product.entity';
 
   controllers: [ProductsController, ProductImagesController],
 
-  providers: [ProductsService, PricingService],
+  providers: [
+    ProductsService,
+    PricingService,
+    ProductCacheInterceptor,
+    {
+      provide: PRODUCT_CACHE,
+      useFactory: () => createCache({ ttl: 60 * 1000 }),
+    },
+  ],
 
   exports: [ProductsService, PricingService, TypeOrmModule],
 })
